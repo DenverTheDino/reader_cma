d9484ad774df31503e34d09999a867169612a3d8
DenverTheDino <christophe.dwwm48@gmail.com>
2024-05-16 16:42:40 +0200

    fvfv

---
+++ magazine_1/formulaire.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'php/functions.php'
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire pour JSON</title>
+</head>
+<body>
+    <h2>Formulaire pour JSON</h2>
+    <form action="formulaire.php" method="post">
+        <label for="titre">Titre :</label>
+        <input type="text" id="title" name="title"><br>
+        
+        <input type="submit" value="Enregistrer le titre">
+    </form>
+</body>
+</html>
